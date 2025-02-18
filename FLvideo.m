ab8fe641ac7983642fd02dbba646b3993c8fa18a
Alfonso Nieto-Castanon <alfnie@gmail.com>
2025-02-18 14:21:53 +0100

    Update FLvideo.m
    bug fix

--- FLvideo.m
+++ FLvideo.m
@@ -962,6 +962,15 @@ else,%odd
    w = [w; flipud(w(1:end-1))];
 end
 end
+function w=flvoice_hamming(n)
+if ~rem(n,2),%even
+    w = .54 - .46*cos(2*pi*(1:n/2)'/(n+1)); 
+    w=[w;flipud(w)];
+else,%odd
+   w = .54 - .46*cos(2*pi*(1:(n+1)/2)'/(n+1));
+   w = [w; flipud(w(1:end-1))];
+end
+end
 
 function [w,t0,e]=harmonicRatio(s,fs,windowlength,overlaplength)
 s2=flvoice_samplewindow(s(:),windowlength,overlaplength,'none','tight');
